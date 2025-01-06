1f170dc699e1fbc0c1ab614b51c72f7b7b5d2bbf
AMorohin <anton.morohin@gmail.com>
2025-01-06 09:44:01 +0300

    get input in signIn

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,9 +1,35 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { Link } from 'react-router-dom'
 import mainIcon from '../../assets/main-icon.png'
 import styles from './SignIn.module.scss'
 
 const SignIn: FC = () => {
+	const [inputStates, setInputStates] = useState<
+		Record<string, { hasText: boolean; touched: boolean }>
+	>({})
+
+	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = event.target
+		setInputStates(prevState => ({
+			...prevState,
+			[name]: {
+				...prevState[name],
+				hasText: value.trim().length > 0,
+			},
+		}))
+	}
+
+	const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
+		const { name } = event.target
+		setInputStates(prevState => ({
+			...prevState,
+			[name]: {
+				...prevState[name],
+				touched: true,
+			},
+		}))
+	}
+
 	return (
 		<section className={styles.signIn}>
 			<Link to='/' className={styles.signIn__button}>
@@ -14,6 +40,64 @@ const SignIn: FC = () => {
 				/>
 			</Link>
 			<h1 className={styles.signIn__title}>Добро пожаловать!</h1>
+			<form action='post' noValidate className={styles.signIn__form}>
+				<fieldset className={styles.signIn__formContainer}>
+					<p className={styles.signIn__formTitle}>Имя:</p>
+					<input
+						type='text'
+						name='firstName'
+						id='firstName'
+						className={`${styles.signIn__formInput} ${
+							inputStates.firstName?.hasText
+								? styles.signIn__formInput_type_active
+								: ''
+						}`}
+						onChange={handleInputChange}
+						onBlur={handleBlur}
+						autoComplete='off'
+						required
+						minLength={2}
+						maxLength={30}
+					/>
+					<span
+						className={`${styles.signIn__inputError} ${
+							inputStates.firstName?.touched && !inputStates.firstName?.hasText
+								? styles.signIn__inputError_type_active
+								: ''
+						}`}
+					>
+						Поле обязательно для заполнения
+					</span>
+				</fieldset>
+				<fieldset className={styles.signIn__formContainer}>
+					<p className={styles.signIn__formTitle}>Фамилия:</p>
+					<input
+						type='text'
+						name='lastName'
+						id='lastName'
+						className={`${styles.signIn__formInput} ${
+							inputStates.lastName?.hasText
+								? styles.signIn__formInput_type_active
+								: ''
+						}`}
+						onChange={handleInputChange}
+						onBlur={handleBlur}
+						autoComplete='off'
+						required
+						minLength={2}
+						maxLength={30}
+					/>
+					<span
+						className={`${styles.signIn__inputError} ${
+							inputStates.lastName?.touched && !inputStates.lastName?.hasText
+								? styles.signIn__inputError_type_active
+								: ''
+						}`}
+					>
+						Поле обязательно для заполнения
+					</span>
+				</fieldset>
+			</form>
 		</section>
 	)
 }
